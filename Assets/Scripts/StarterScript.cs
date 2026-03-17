640bbe68cad1fa24894a7e10304389d80e213846
Steve Falcigno <steve1281@hotmail.com>
2026-03-17 15:47:45 -0400

    comment out cursor lock, add buttons, handle buttons

--- Assets/Scripts/StarterScript.cs
+++ Assets/Scripts/StarterScript.cs
@@ -11,12 +11,17 @@ public class StarterScript : MonoBehaviour
     [SerializeField] GameObject myGate;
     [SerializeField] GameObject name1;
     [SerializeField] GameObject name2;
+    [SerializeField] GameObject gateButton;
 
     void Start()
     {
         myNumber = 4;
         myName = "Steve";
         myChoice = true;
+
+       // Unlock and show the cursor
+       // Cursor.lockState = CursorLockMode.None;
+       // Cursor.visible = true;
     }
 
     void Update()
@@ -26,13 +31,22 @@ public class StarterScript : MonoBehaviour
             //name1.GetComponent<TMP_Text>().text = myName;
             name1.GetComponent<TMP_Text>().text = $"My Number: {myNumber} My Name: {myName}";
             name2.GetComponent<TMP_Text>().text = $"";
-            myGate.SetActive(true);
+            //myGate.SetActive(true);
         } else
         {
-            myGate.SetActive(false);
+            //myGate.SetActive(false);
             name1.GetComponent<TMP_Text>().text = $"";
             name2.GetComponent<TMP_Text>().text = $"My Number: {myNumber} My Name: {myName}";
         }
         
+    }
+
+    public void OpenGate()
+    {
+        myGate.GetComponent<Animator>().Play("GateSwing");
+    }
+    public void HideButton()
+    {
+        gateButton?.SetActive(false);
     }
 }
